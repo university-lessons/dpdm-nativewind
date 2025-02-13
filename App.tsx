407a13e24b6bb292b5064a368eabca1db6c65872
andresjesse <andres.porfirio.tablet@gmail.com>
2025-02-13 14:28:39 -0300

    sdk 52 base project

--- App.tsx
+++ App.tsx
@@ -1,5 +1,20 @@
-import Home from "./app/Home";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return <Home />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
